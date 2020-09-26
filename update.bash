fc0d757b77bb83d1de5cca3fba2e965bb5386282
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-09-26 22:29:58 +0900

    download: Add commants

--- update.bash
+++ update.bash
@@ -4,9 +4,8 @@ TAR_DIR="tmp_for_textlint"
 USER="dbgroup-nagoya-u"
 REPOSITORY="test-public-textlint-settings"
 
-REPOSITORY_URL="https://github.com/${USER}/${REPOSITORY}"
 
-# Package check
+# Check if commands exist.
 ESC=$(printf '\033')
 RED="${ESC}[31m"
 if ! command -v wget &> /dev/null
@@ -27,12 +26,16 @@ curl --silent https://api.github.com/repos/${USER}/${REPOSITORY}/tags > res.json
 tag_latest=$(jq '.[] | .name' res.json | sort -rV | head -n1 | sed 's/"//g')
 latest_file="${tag_latest}.tar.gz"
 rm res.json
-wget ${REPOSITORY_URL}/archive/${latest_file}
 
+repository_url="https://github.com/${USER}/${REPOSITORY}"
+wget ${repository_url}/archive/${latest_file}
+
+# Untar files in |TAR_DIR|
 mkdir ${TAR_DIR} && tar -zxvf ${latest_file} -C ${TAR_DIR} --strip-components 1
 rm ${latest_file}
 
 (
+  # Sync files
   pushd ${TAR_DIR}
   dirarray=($(find . -mindepth 1 -type d))
 
@@ -42,12 +45,14 @@ rm ${latest_file}
   done
 
   pushd ${TAR_DIR}
-  for file in $(find . -type d \( -path './.git' -o -path './dir' \) -prune -false -o -type f -not -name 'README.md' -not -name 'paper.txt'); do
+  # TODO: Clarify exclude file and directory. It'd be broken when *.md files are needed.
+  for file in $(find . -type d \( -path './.git' -o -path './dir' \) -prune -false -o -type f -not -name '**.md' -not -name 'paper.txt' -not -name 'update.bash'); do
     mv ${file} ../${file}
   done
 )
 rm -rf ${TAR_DIR}
 
+# TODO: Error handling
 ESC=$(printf '\033')
 GREEN="${ESC}[32m"
 printf "${GREEN}%s${ESC}[m\n" 'Update paper-lint settings successfully!'
