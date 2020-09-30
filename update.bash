7f4583258dc37f1f99a4dad3e4ea95ec94947490
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-09-30 22:21:11 +0900

    Clarify message in updating

--- update.bash
+++ update.bash
@@ -5,54 +5,59 @@
 USER="dbgroup-nagoya-u"
 REPOSITORY="paper-lintrules"
 
-# Check if commands exist.
 ESC=$(printf '\033')
 RED="${ESC}[31m"
+GREEN="${ESC}[32m"
+BLUE="${ESC}[34m"
 
+# Check if commands exist.
 if ! command -v wget &>/dev/null;
 then
-  echo "wget could not be found."
+  printf "wget could not be found.\n"
   printf "Type ${RED}%s${ESC}[m\n" 'sudo apt install wget'
   exit 1
 fi
 
 if ! command -v jq &>/dev/null;
 then
-  echo "jq could not be found."
+  printf "jq could not be found.\n"
   printf "Type ${RED}%s${ESC}[m\n" 'sudo apt install jq'
   exit 1
 fi
 
 # Download from the latest commit on master branch.
 latest_file_url="https://github.com/${USER}/${REPOSITORY}/archive/master.zip"
-wget ${latest_file_url}
+wget -q ${latest_file_url} --show-progress
 latest_file="master.zip"
 
 # Unzip files in |unzip_dir|.
 unzip_dir="${REPOSITORY}-master"
-unzip ${latest_file}
+unzip -qq ${latest_file}
 rm ${latest_file}
 
 # Sync files.
 (
-  pushd ${unzip_dir}
+  pushd ${unzip_dir} > /dev/null
   dirarray=($(find . -mindepth 1 -type d))
 
-  popd
+  popd > /dev/null
   for dirname in ${dirarray[@]}; do
     mkdir -p ${dirname}
   done
 
-  pushd ${unzip_dir}
+  pushd ${unzip_dir} > /dev/null
   # TODO: Clarify exclude file and directory.
-  for file in $(find . -type d \( -path './.github/ISSUE_TEMPLATE' -o -path './dir' \) -prune -false -o -type f -not -name 'README.md' -not -name 'paper.tex' -not -name 'update.bash');
+  for file in $(find . -type d \( -path './.github/ISSUE_TEMPLATE' -o -path './dir' \) -prune -false -o -type f -not -name 'README.md' -not -name '**.tex' -not -name 'update.bash');
   do
-    mv ${file} ../${file}
+    diff -N ${file} ../${file} > /dev/null
+    if [ $? -ne 0 ] ;
+    then
+      mv ${file} ../${file}
+      printf "${BLUE}${file}${ESC}[m %s\n" 'has been updated.'
+    fi
   done
 )
 rm -rf ${unzip_dir}
 
 # TODO: Error handling
-ESC=$(printf '\033')
-GREEN="${ESC}[32m"
 printf "${GREEN}%s${ESC}[m\n" 'Update paper-lintrules settings successfully!'
