0876d567b9614b569973fc7530da9f4e09db8210
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-10-02 20:54:06 +0900

    Disable draftcheck

--- lintrules/draftcheck/rules.py
+++ lintrules/draftcheck/rules.py
@@ -82,7 +82,7 @@ def rule_generator(show_spaces=False, in_env='paragraph'):
     return inner_rule
 
 
-@rule(r'\s+\\footnote{', show_spaces=True)
+#@rule(r'\s+\\footnote{', show_spaces=True)
 def check_space_before_footnote(text, matches):
     """Do not precede footnotes with spaces.
 
@@ -101,7 +101,7 @@ def check_space_before_footnote(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\.\\cite{')
+#@rule(r'\.\\cite{')
 def check_cite_after_period(text, matches):
     """Place citations before periods with a non-breaking space.
 
@@ -118,7 +118,7 @@ def check_cite_after_period(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\b(:?in|as|on|by)[ ~]\\cite{')
+#@rule(r'\b(:?in|as|on|by)[ ~]\\cite{')
 def check_cite_used_as_noun(text, matches):
     """Avoid using citations as nouns.
 
@@ -133,7 +133,7 @@ def check_cite_used_as_noun(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'[^~]\\cite{')
+#@rule(r'[^~]\\cite{')
 def check_no_space_before_cite(text, matches):
     """Place a single, non-breaking space '~' before citations.
 
@@ -150,7 +150,7 @@ def check_no_space_before_cite(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'[^~]\\ref{')
+#@rule(r'[^~]\\ref{')
 def check_no_space_before_ref(text, matches):
     """Place a single, non-breaking space '~' before references.
 
@@ -165,7 +165,7 @@ def check_no_space_before_ref(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\d+%')
+#@rule(r'\d+%')
 def check_unescaped_percentage(text, matches):
     """Escape percentages with backslash.
 
@@ -180,7 +180,7 @@ def check_unescaped_percentage(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\s[,;.!?]', show_spaces=True)
+#@rule(r'\s[,;.!?]', show_spaces=True)
 def check_space_before_punctuation(text, matches):
     """Do not precede punctuation characters with spaces.
 
@@ -197,7 +197,7 @@ def check_space_before_punctuation(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\w+\(|\)\w+', show_spaces=True)
+#@rule(r'\w+\(|\)\w+', show_spaces=True)
 def check_no_space_next_to_parentheses(text, matches):
     """Separate parentheses from text with a space.
 
@@ -212,7 +212,7 @@ def check_no_space_next_to_parentheses(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\d+\s?x\d+')
+#@rule(r'\d+\s?x\d+')
 def check_incorrect_usage_of_x_as_times(text, matches):
     """In the context of 'times' or 'multiply', use $\\times$ instead of 'x'.
 
@@ -227,7 +227,7 @@ def check_incorrect_usage_of_x_as_times(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'[a-z]+\s-\s[a-z]+')
+#@rule(r'[a-z]+\s-\s[a-z]+')
 def check_space_surrounded_dash(text, matches):
     """Use an em-dash '---' to denote parenthetical breaks or statements.
 
@@ -242,7 +242,7 @@ def check_space_surrounded_dash(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\b([a-z]+)\s+\1\b(?![^{]*})')
+#@rule(r'\b([a-z]+)\s+\1\b(?![^{]*})')
 def check_duplicate_word(text, matches):
     """Remove duplicated word.
 
@@ -259,7 +259,7 @@ def check_duplicate_word(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\.\.\.')
+#@rule(r'\.\.\.')
 def check_dot_dot_dot(text, matches):
     """Typeset ellipses by \\ldots, not '...'.
 
@@ -274,7 +274,7 @@ def check_dot_dot_dot(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'"')
+#@rule(r'"')
 def check_double_quote(text, matches):
     """Use left and right quotation marks `` and '' rather than ".
 
@@ -289,7 +289,7 @@ def check_double_quote(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\s\'.+?\'[\s\.,]')
+#@rule(r'\s\'.+?\'[\s\.,]')
 def check_single_quote(text, matches):
     """Use left and right quotation marks ` and ' rather than '.
 
@@ -304,7 +304,7 @@ def check_single_quote(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\\begin{center}', in_env='any')
+#@rule(r'\\begin{center}', in_env='any')
 def check_begin_center(text, matches):
     """Use \\centering instead of \\begin{center}.
 
@@ -326,7 +326,7 @@ def check_begin_center(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'^\$\$', in_env='math')
+#@rule(r'^\$\$', in_env='math')
 def check_double_dollar_math(text, matches):
     """Use \\[ or \\begin{equation} instead of $$.
 
@@ -346,7 +346,7 @@ def check_double_dollar_math(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\d\s?-\s?\d')
+#@rule(r'\d\s?-\s?\d')
 def check_numeric_range_dash(text, matches):
     """Use endash '--' for numeric ranges instead of hyphens.
 
@@ -361,7 +361,7 @@ def check_numeric_range_dash(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\\footnote{.+?}[,;.?]')
+#@rule(r'\\footnote{.+?}[,;.?]')
 def check_footnote_before_punctuation(text, matches):
     """Place footnotes after punctuation marks.
 
@@ -378,7 +378,7 @@ def check_footnote_before_punctuation(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'<[^\s](.+?)[^\s]>', in_env='math')
+#@rule(r'<[^\s](.+?)[^\s]>', in_env='math')
 def check_relational_operators(text, matches):
     """Use \\langle and \\rangle instead of '<' and '>' for angle brackets.
 
@@ -396,7 +396,7 @@ def check_relational_operators(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\\cite{.+?}\s?\\cite{')
+#@rule(r'\\cite{.+?}\s?\\cite{')
 def check_multiple_cite(text, matches):
     """Use \\cite{..., ...} for multiple citations.
 
@@ -411,7 +411,7 @@ def check_multiple_cite(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\d(m|A|kg|s|K|mol|cd)\b')
+#@rule(r'\d(m|A|kg|s|K|mol|cd)\b')
 def check_number_next_to_unit(text, matches):
     """Place a non-breaking space between a number and its unit.
 
@@ -426,7 +426,7 @@ def check_number_next_to_unit(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'[^\\](sin|cos|tan|log|max|min)', in_env='math')
+#@rule(r'[^\\](sin|cos|tan|log|max|min)', in_env='math')
 def check_unescaped_named_math_operators(text, matches):
     """Precede named mathematical operators with a backslash.
 
@@ -441,7 +441,7 @@ def check_unescaped_named_math_operators(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\b(e\.g\.|i\.e\.)\s+')
+#@rule(r'\b(e\.g\.|i\.e\.)\s+')
 def check_abbreviation_innerword_spacing(text, matches):
     """Place a '\\ ' (backslash space) after the period of an abbreviation.
 
@@ -458,49 +458,49 @@ def check_abbreviation_innerword_spacing(text, matches):
     return [m.span() for m in matches]
 
 
-@rule(r'\\def\\[a-z]+{')
+#@rule(r'\\def\\[a-z]+{')
 def check_def_command(text, matches):
     """Do not use the \\def command. Use \\newcommand instead."""
     return [m.span() for m in matches]
 
 
-@rule(r'\\sloppy')
+#@rule(r'\\sloppy')
 def check_sloppy_command(text, matches):
     """Avoid the \\sloppy command."""
     return [m.span() for m in matches]
 
 
-@rule(r"'''|```")
+#@rule(r"'''|```")
 def check_triple_quote(text, matches):
     r"""Use a thin space \, to separate quotes."""
     return [m.span() for m in matches]
 
 
-@rule(r'1st|2nd|3rd')
+#@rule(r'1st|2nd|3rd')
 def check_unspelt_ordinal_numbers(text, matches):
     """Spell out ordinal numbers (1st, 2nd, etc.) in words."""
     return [m.span() for m in matches]
 
 
-@rule(r'[a-z]+ \d [a-z]+')
+#@rule(r'[a-z]+ \d [a-z]+')
 def check_unspelt_single_digit_numbers(text, matches):
     """Spell out single digit numbers in words."""
     return [m.span() for m in matches]
 
 
-@rule(r',\s*\.\.\.\s*,', in_env='math')
+#@rule(r',\s*\.\.\.\s*,', in_env='math')
 def check_dot_dot_dot_maths(text, matches):
     """Use \\cdots to denote ellipsis in maths."""
     return [m.span() for m in matches]
 
 
-@rule(r'(?<!\\url{)(\bhttps?://)[^\s.]+\.[-A-Za-z0-9+&@#/%?=~_|!:,.;]+')
+#@rule(r'(?<!\\url{)(\bhttps?://)[^\s.]+\.[-A-Za-z0-9+&@#/%?=~_|!:,.;]+')
 def check_bare_urls(text, matches):
     """Wrap URLs with the \\url command."""
     return [m.span() for m in matches]
 
 
-@rule(r'\.  [A-Z]')
+#@rule(r'\.  [A-Z]')
 def check_double_space(text, matches):
     """Prefer single space over double space after a period."""
     return [m.span() for m in matches]
