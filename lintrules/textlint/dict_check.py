b5a9d5e4eab92967ae6a68223c8b7f1cc48bfb66
syunya suzuki <syunya_0617@outlook.jp>
2020-11-20 23:54:56 +0900

    Fix existed dictionarys and Add findng duplicate/conflict program

---
+++ lintrules/textlint/dict_check.py
@@ -0,0 +1,66 @@
+import re
+from collections import defaultdict
+#↓既存の辞書（検査されない）
+exsisted_path = ["WEB+DB_PRESS.yml"]#,"techbooster.yml","textlint-prh-kanji-hiragana.yml"]
+
+flag = False
+expecteds = set()
+patterns = set()
+
+for p in exsisted_path:
+    with open(p) as f:
+        for i in f:
+            if re.match(".*expected:",i):
+                expecteds.add(i.split()[-1])
+                flag = False
+            elif re.match(".*patterns?:",i):
+                if re.match('patterns?:',i.split()[-1]):
+                    flag = True
+                else:
+                    patterns.add(re.split("[:|-]\s+",i)[-1].split('\n')[0])
+                    flag = False
+            elif not(re.match("\s+\-",i)):
+                flag = False
+            elif flag:
+                patterns.add(re.split("[:|-]\s+",i)[-1].split('\n')[0])
+#↓追加する辞書
+new_path = ["techbooster.yml","textlint-prh-noun.yml","textlint-prh-kanji-hiragana.yml"]
+line = 1
+flag = False
+
+for p in new_path:
+    line = 1
+    with open(p) as f:
+        for i in f:
+            if re.match(".*expected:",i):
+                flag = False
+                ex = re.split("[:-]\s+",i)[-1].split('\n')[0]
+                if ex in patterns:
+                    print("\033[31m","Found CONFLICTED expected in",p,"at line",line,":",ex,"\033[0m")
+                elif ex in expecteds:
+                    print("Found duplicated expected in",p,"at line",line,":",ex)
+                else:
+                    expecteds.add(ex)
+            elif re.match(".*patterns?:",i):
+                if re.match('patterns?:',i.split()[-1]):
+                    flag = True
+                else:
+                    flag = False
+                    pt = re.split("[:-]\s+",i)[-1].split('\n')[0]
+                    if pt in expecteds:
+                        print("\033[31m","Found CONFLICTED pattern  in",p,"at line",line,":",pt,"\033[0m")
+                    elif pt in patterns:
+                        print("Found duplicated pattern  in",p,"at line",line,":",pt)
+                    else:
+                        patterns.add(pt)
+            elif not(re.match("\s+\-",i)):
+                flag != False
+            elif flag:
+                pt = i.split()[-1]
+                if pt in expecteds:
+                    print("\033[31m","Found CONFLICTED pattern in",p,"at line",line,":",pt,"\033[0m")
+                elif pt in patterns:
+                    print("Found duplicated pattern in",p,"at line",line,":",pt)
+                else:
+                    patterns.add(pt)
+            line += 1
