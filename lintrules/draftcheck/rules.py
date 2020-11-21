2d2c472e4acbcbb7a5791a083f63116a32b6b338
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-11-21 20:54:43 +0900

    半角括弧

--- lintrules/draftcheck/rules.py
+++ lintrules/draftcheck/rules.py
@@ -432,7 +432,7 @@ def check_bare_urls(text, matches):
     """Wrap URLs with the \\url command."""
     return [m.span() for m in matches]
 
-@rule(r"\(\\ref{")
+@rule(r"[\(（]\\ref{")
 def check_bracket_ref(text, matches):
     """\eqrefを使うと自動で括弧がつきます．"""
     return [m.span() for m in matches]
