908342ff9e2141083b3bb6205a36f84a15d8e0d3
nrkt <nu.noken2018@gmail.com>
2020-11-27 15:59:56 +0900

    Check

--- lintrules/bibtex/check.sh
+++ lintrules/bibtex/check.sh
@@ -1,1 +1,10 @@
-grep -P -rn '(?<!\\url{)(\bhttps?://)[^\s.]+\.[-A-Za-z0-9+&@#/%?=~_|!:,.;]+' * | grep .bib | cut -d: -f1,2 | sed -e "s/\$/ Wrap URLs with the \\\\url command./" | reviewdog -efm="%f:%l %m" -name="bibtexlint" -reporter=github-pr-review
+grep -P -rn '(?<!\\url{)(\bhttps?://)[^\s.]+\.[-A-Za-z0-9+&@#/%?=~_|!:,.;]+' * \
+| grep .bib \
+| cut -d: -f1,2 \
+| sed -e "s|\$| URLは\`\\\url\` コマンドで囲います.|"
+
+grep -P -rn 'pages[ ,　]*?=.*?[0-9][ ,　]*?-[ ,　]*?[0-9]' * \
+| grep .bib \
+| cut -d: -f1,2 \
+| sed -e "s|\$| 数値範囲は\`--\` を使用します.|"
+
